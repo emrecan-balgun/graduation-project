22cc5193a5198838eb9e206d5e14ba6d4f7bcd6c
İpek Soydemir <65809521+ipeksoydemir@users.noreply.github.com>
2021-12-27 10:05:17 +0300

    Timer +  Chest Sys. Added, PlayerMovement Update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,22 +5,38 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private UnityEngine.CharacterController controller;
-
+    public VariableJoystick variableJoystick;
+    public RectTransform handle;
     public float rotateSpeed = 2f, speed=6f;
     public int myScore = 0;
+    private Rigidbody rb;
     void Start()
     {
-       controller= GetComponent<CharacterController>();
-       
+       //controller= GetComponent<CharacterController>();
+       rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0,Input.GetAxis("Horizontal")*rotateSpeed,0);
-        float curSpeed = speed * Input.GetAxis("Vertical");
-        controller.SimpleMove( curSpeed * transform.TransformDirection(Vector3.forward) *10f );
-        print("MyScore:"+myScore);
+        transform.Rotate(0,variableJoystick.Horizontal*rotateSpeed,0);
+        transform.Translate(0,0,speed * variableJoystick.Vertical*Time.deltaTime);
+
+
+        Vector2 dir = variableJoystick.Direction;
+        if (dir.x > 0)
+        {
+            handle.Rotate(0,0,dir.x*5) ;
+        }
+        else
+        {
+            handle.Rotate(0,0,dir.x*5) ;
+        }
+        
+        /* transform.Rotate(0,Input.GetAxis("Horizontal")*rotateSpeed,0);
+         float curSpeed = speed * Input.GetAxis("Vertical");
+         controller.SimpleMove( curSpeed * transform.TransformDirection(Vector3.forward) *10f );*/
+
     }
     
     void OnCollisionEnter(Collision collision)
@@ -34,6 +50,14 @@ public class PlayerMovement : MonoBehaviour
         if (collision.gameObject.CompareTag("mutant"))
         {
             myScore--;
+        }
+
+        if (collision.gameObject.CompareTag("chest"))
+        {
+            print("ups");
+           
+            collision.gameObject.transform.position = ChestManager.SetRandomPosition();
+            myScore = myScore + collision.gameObject.GetComponent<Chest>().feature;
         }
     }
 }
